bdcc5f73493fdd9cfad1d7827c628f7c2117ef44
pankaj kumar <pankajsingh92533@gmail.com>
2024-02-05 15:12:19 +0530

    done redux od adding users

--- my-app/src/app/components/DisplayUsers.js
+++ my-app/src/app/components/DisplayUsers.js
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-const DisplayUsers = () => {
+export default function DisplayUsers() {
+  const userData = useSelector((data) => data.users);
+  console.log(userData);
+
   return (
-    <div>displaying  users</div>
-  )
+    <div>
+      <h3>Users List</h3>
+      {
+        userData.map((item) => (
+          <div key={item.id} >
+            {item.name}
+          </div>
+        ))
+      }
+    </div>
+  );
 }
-
-export default DisplayUsers
